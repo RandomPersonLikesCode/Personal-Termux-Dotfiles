517181160039f1b0be3d5f9ea8b971d014c86be3
RandomPerson <bagas.gwlt@gmail.com>
2026-02-23 17:05:05 +0700

    New: added bootstrap lua script to easily setup symlinks

---
+++ bootstrap.lua
@@ -0,0 +1,41 @@
+#!/data/data/com.termux/files/usr/bin/env lua
+
+local home_dir = os.getenv("HOME")
+local symlink_command = "ln -sfnT "
+
+local target_names = {
+  home_dir .. "/.dotfiles/.termux ",
+  home_dir .. "/.dotfiles/.zsh ",
+  home_dir .. "/.dotfiles/.config/helix ",
+  home_dir .. "/.dotfiles/.config/fastfetch ",
+
+  home_dir .. "/.dotfiles/.zshrc ",
+  home_dir .. "/.dotfiles/.hushlogin ",
+  home_dir .. "/.dotfiles/.clang-format ",
+  home_dir .. "/.dotfiles/.stylua.toml ",
+}
+
+local symlink_names = {
+  home_dir .. "/.termux",
+  home_dir .. "/.zsh",
+  home_dir .. "/.config/helix",
+  home_dir .. "/.config/fastfetch",
+
+  home_dir .. "/.zshrc",
+  home_dir .. "/.hushlogin",
+  home_dir .. "/.clang-format",
+  home_dir .. "/.stylua.toml",
+}
+
+function set_symlink(target_name, symlink_name)
+  os.execute(symlink_command .. target_name .. symlink_name)
+end
+
+if #symlink_names ~= #target_names then
+  print("Source and target counts are not the same, exiting to avoid error")
+  os.exit(1)
+else
+  for i = 1, #target_names do
+    set_symlink(target_names[i], symlink_names[i])
+  end
+end
