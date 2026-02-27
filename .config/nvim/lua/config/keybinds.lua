67623140415407d7e1f56a864248e7fb5b3fc8a9
RandomPerson <bagas.gwlt@gmail.com>
2026-02-28 01:06:21 +0700

    chore: adding several appearance plugins

--- .config/nvim/lua/config/keybinds.lua
+++ .config/nvim/lua/config/keybinds.lua
@@ -3,15 +3,15 @@ local tcb = require("telescope.builtin")
 local ls = require("luasnip")
 
 map("n", "<leader>Ll", "<Cmd>Lazy<CR>", {
-  desc = "Open Lazy.nvim",
+  desc = "Open package manager",
 })
 
 map("n", "<leader>Ls", "<Cmd>Lazy sync<CR>", {
-  desc = "Sync Lazy.nvim",
+  desc = "Synch packages",
 })
 
 map("n", "<leader>Lu", "<Cmd>Lazy update<CR>", {
-  desc = "Perform update",
+  desc = "Update packages",
 })
 
 map("n", "<leader>term", function()
@@ -20,7 +20,7 @@ map("n", "<leader>term", function()
   vim.cmd.wincmd("J")
   vim.api.nvim_win_set_height(0, 10)
 end, {
-  desc = "Open Terminal",
+desc = "Open Terminal",
 })
 
 map("n", "<leader>src", "<Cmd>luafile %<CR>", {
@@ -39,40 +39,41 @@ map("n", "<leader>ch", ":checkhealth ", {
   desc = "Perform check health",
 })
 
-map("n", "<leader>ex", "<cmd>Oil<CR>", {
+map("n", "<leader>ex", "<cmd>Ex<CR>", {
   desc = "Open file manager",
 })
 
 map("i", "<C-k>", function()
   ls.expand()
 end, {
-  silent = true,
-  desc = "Expand Snippet",
+silent = true,
+desc = "Expand Snippet",
 })
 
 map({ "i", "s" }, "<C-l>", function()
   ls.jump(1)
 end, {
-  silent = true,
-  desc = "Jump to Next Snippet Placeholder",
+silent = true,
+desc = "Jump to Next Snippet Placeholder",
 })
 
 map({ "i", "s" }, "<C-h>", function()
   ls.jump(-1)
 end, {
-  silent = true,
-  desc = "Jump to Previous Snippet Placeholder",
+silent = true,
+desc = "Jump to Previous Snippet Placeholder",
 })
 
 map({ "n", "v", "c" }, "<leader>dd", function()
   print(vim.diagnostic.setloclist())
 end, {
-  silent = true,
-  desc = "Expand Diagnostic Warnings/Errors List",
+silent = true,
+desc = "Expand Diagnostic Warnings/Errors List",
 })
 
-map("n", "H", function()
-  vim.lsp.buf.hover()
-end, {
-  desc = "Additional Keymap for LSP Hover Documentation",
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function(args)
+    require("conform").format({ bufnr = args.buf })
+  end,
 })
--- .config/nvim/lua/plugins/appearances.lua
+++ .config/nvim/lua/plugins/appearances.lua
@@ -24,4 +24,64 @@ return {
       },
     },
   },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    event = "VeryLazy",
+
+    config = function()
+      local lualine = require("lualine")
+
+      lualine.setup({
+        options = {
+          icons_enabled = true,
+          theme = 'auto',
+          component_separators = { left = '|', right = '|'},
+          section_separators = { left = '', right = ''},
+          always_divide_middle = true,
+          always_show_tabline = true,
+          globalstatus = false,
+          refresh = {
+            statusline = 100,
+            tabline = 100,
+            winbar = 100,
+          }
+        },
+        sections = {
+          lualine_a = {'mode'},
+          lualine_b = {'branch', 'diff', 'diagnostics'},
+          lualine_c = {'filename'},
+          lualine_x = {'fileformat', {
+            'encoding',
+            fmt = function(str)
+              return str:upper() or "N/A"
+            end
+          }, 'filetype'},
+          lualine_y = {'progress'},
+          lualine_z = {'location'}
+        },
+        inactive_sections = {
+          lualine_c = {'filename'},
+          lualine_x = {'location'},
+        },
+      })
+    end
+  },
+  {
+    "nvim-mini/mini.tabline",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons"
+    },
+    version = "*",
+    event = "VeryLazy",
+
+    opts = {}
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    event = "VeryLazy",
+
+    opts = {},
+  },
 }
